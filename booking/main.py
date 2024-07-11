254d27a02fc1b7ef63133c09ae4efb6238b4a741
THARUN RAJ R <tharunrajr2004@gmail.com>
2024-07-11 17:40:27 +0530

    backend

--- booking/main.py
+++ booking/main.py
@@ -5,8 +5,10 @@ from sqlalchemy.orm import Session
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
+from . import auth
 
 app = FastAPI()
+app.include_router(auth.router)
 
 app.mount("/static", StaticFiles(directory="booking/static"), name="static")
 
@@ -23,16 +25,48 @@ def get_db():
         db.close()
 
 
-@app.get("/")
-def root():
-    return {"message": "bus booking"}
-
-
-@app.get("/bus/")
+@app.get("/buses/")
 def all(db: Session = Depends(get_db)):
     try:
-        bus = db.query(models.Bus).all()
-        return {"bus": bus}
+        buses = db.query(models.Bus).all()
+        return buses
+    except Exception as e:
+        return {"error": str(e)}
+
+
+@app.get("/routes/")
+def all(db: Session = Depends(get_db)):
+    try:
+        routes = db.query(models.Route).all()
+        return routes
+    except Exception as e:
+        return {"error": str(e)}
+
+
+@app.get("/buses/search")
+def search(source: str, destination: str, db: Session = Depends(get_db)):
+    try:
+        routes = (
+            db.query(models.Route)
+            .filter(
+                models.Route.source == source
+                and models.Route.destination == destination
+            )
+            .all()
+        )
+        return routes
+    except Exception as e:
+        return {"error": str(e)}
+
+
+@app.post("/bus/booking")
+def bookings(route_id: int, seat_number: int, db: Session = Depends(get_db)):
+    try:
+        booking = models.Booking(route_id=route_id, seat_number=seat_number)
+        db.add(booking)
+        db.commit()
+        db.refresh(booking)
+        return booking
     except Exception as e:
         return {"error": str(e)}
 
@@ -43,5 +77,27 @@ def delete(db: Session = Depends(get_db)):
     for bus in buses:
         del_bus = db.query(models.Bus).filter(models.Bus.id == bus.id).first()
         db.delete(del_bus)
+        db.commit()
+    return "deleted"
+
+
+@app.delete("/route/")
+def delete(db: Session = Depends(get_db)):
+    routes = db.query(models.Route).all()
+    for route in routes:
+        del_route = db.query(models.Route).filter(models.Route.id == route.id).first()
+        db.delete(del_route)
+        db.commit()
+    return "deleted"
+
+
+@app.delete("/booking/")
+def delete(db: Session = Depends(get_db)):
+    bookings = db.query(models.Booking).all()
+    for booking in bookings:
+        del_booking = (
+            db.query(models.Booking).filter(models.Booking.id == booking.id).first()
+        )
+        db.delete(del_booking)
         db.commit()
     return "deleted"
--- booking/models.py
+++ booking/models.py
@@ -30,11 +30,19 @@ class Route(Base):
 class Booking(Base):
     __tablename__ = "booking"
 
-    user_id = Column(String, primary_key=True, index=False)
+    id = Column(Integer, primary_key=True, index=True)
     route_id = Column(Integer, ForeignKey("route.id"))
     seat_number = Column(Integer)
 
     route = relationship("Route", back_populates="bookings")
+
+
+class Users(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True)
+    hashded_password = Column(String)
 
 
 Base.metadata.create_all(engine)
@@ -55,43 +63,43 @@ if db.query(Bus).count() == 0:
     id_3 = bus_3.id
 
     route_1 = Route(
-        source="Station A",
-        destination="Station B",
+        source="A",
+        destination="B",
         bus_id=id_1,
         departure_time=800,
         arrival_time=1000,
     )
     route_2 = Route(
-        source="Station C",
-        destination="Station D",
+        source="C",
+        destination="D",
         bus_id=id_1,
         departure_time=1200,
         arrival_time=1400,
     )
     route_3 = Route(
-        source="Station A",
-        destination="Station B",
+        source="A",
+        destination="B",
         bus_id=id_2,
         departure_time=900,
         arrival_time=1100,
     )
     route_4 = Route(
-        source="Station C",
-        destination="Station D",
+        source="C",
+        destination="D",
         bus_id=id_2,
         departure_time=1300,
         arrival_time=1500,
     )
     route_5 = Route(
-        source="Station A",
-        destination="Station B",
+        source="A",
+        destination="B",
         bus_id=id_3,
         departure_time=1000,
         arrival_time=1200,
     )
     route_6 = Route(
-        source="Station C",
-        destination="Station D",
+        source="C",
+        destination="D",
         bus_id=id_3,
         departure_time=1400,
         arrival_time=1600,
